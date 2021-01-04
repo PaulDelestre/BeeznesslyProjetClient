bb6c3cbc5510270f4824aa767c7b4901dfa04ecd
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2021-01-04 12:25:33 +0100

    split the registration into two parts : one for expert and one for entrepreneur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,22 +5,29 @@ namespace App\Form;
 use App\Entity\User;
 use App\Entity\TypeOfUser;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::Class, [
+                'label' => false,
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Email'
+                    ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -36,28 +43,28 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-            ->add('typeOfUser', EntityType::class, [
-                'class' => TypeOfUser::class,
-                'label' => 'Je suis un.e ',
-                'choice_label' => 'name',
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Mot de passe'
+                    ]
             ])
             ->add('firstname', TextType::class, [
                 'label' => false,
                 'required' => false,
                 'attr' => [
-                    'placeholder' => 'Firstname'
+                    'placeholder' => 'Prénom'
                     ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => false,
                 'required' => false,
                 'attr' => [
-                    'placeholder' => 'Lastname'
+                    'placeholder' => 'Nom'
                     ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => "J'accepte les CDG",
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
