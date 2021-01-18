d661f319f420796a0bcf5a8461ed9f9176daf57c
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2021-01-18 22:32:07 +0100

    i did the dashboard of ebooks downloaded for entrepreneur

--- src/Controller/EntrepreneurController.php
+++ src/Controller/EntrepreneurController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Entity\Contact;
+use App\Repository\DownloadRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use App\Entity\User;
-use App\Entity\Contact;
-use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/entrepreneur", name="entrepreneur_")
@@ -98,7 +99,7 @@ class EntrepreneurController extends AbstractController
       /**
      * @Route("/ebook", methods={"GET"}, name="ebook")
      */
-    public function ebooks(): Response
+    public function ebooks(DownloadRepository $donwloadRepository): Response
     {
         $user = $this->getUser();
         if ($user->getIsValidated() == false) {
@@ -107,8 +108,10 @@ class EntrepreneurController extends AbstractController
             ]);
         }
 
+        $downloads = $donwloadRepository->findBy(['user' => $user]);
+
         return $this->render('entrepreneur/ebook.html.twig', [
-            'ebooks' => $user->getEbooks(),
+            'downloads' => $downloads,
             'user' => $user = $this->getUser()
         ]);
     }
