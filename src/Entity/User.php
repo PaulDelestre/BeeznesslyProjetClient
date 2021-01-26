e48e588652837a3b5815ce18297a658fd9a12823
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2021-01-26 14:36:58 +0100

    fix bug on image constraint and added one to the logo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -125,6 +126,11 @@ class User implements UserInterface, \Serializable
 
     /**
     * @Vich\UploadableField(mapping="logo_file", fileNameProperty="logo")
+    * @Assert\File(
+    *     maxSize = "3000k",
+    *     mimeTypes = {"image/png", "image/jpeg"},
+    *     mimeTypesMessage = "Seuls les formats jpg, jpeg et png sont acceptés"
+    * )
     * @var File
     */
     private $logoFile;
