a764b2f3653d6e0ab13b9472c59821502e204dbe
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2021-01-27 14:13:06 +0100

    add fixtures to fake downloads of ebooks

--- src/DataFixtures/EbookFixtures.php
+++ src/DataFixtures/EbookFixtures.php
@@ -22,17 +22,31 @@ class EbookFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
-        for ($i = 0; $i < 50; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $ebook = new Ebook();
             $ebook->setTitle($faker->words(3, true));
             $ebook->setDescription($faker->paragraph());
             $ebook->setReleaseDate(new \DateTime($faker->date('Y-m-d', 'now')));
             $ebook->setEditorName($faker->word());
             $ebook->setAuthor($faker->name());
-            $ebook->setIsValidated($faker->numberBetween(0, 1));
+            $ebook->setIsValidated(true);
             $ebook->setSlug($this->slugifyService->generate($ebook->getTitle()));
             $ebook->setExpertise($this->getReference('expertise_' . rand(0, 5)));
-            $ebook->setUser($this->getReference('user_' . rand(0, 9)));
+            $ebook->setUser($this->getReference('user_' . rand(0, 4)));
+            $manager->persist($ebook);
+            $this->addReference('ebook_' . $i, $ebook);
+        }
+        for ($i = 10; $i < 20; $i++) {
+            $ebook = new Ebook();
+            $ebook->setTitle($faker->words(3, true));
+            $ebook->setDescription($faker->paragraph());
+            $ebook->setReleaseDate(new \DateTime($faker->date('Y-m-d', 'now')));
+            $ebook->setEditorName($faker->word());
+            $ebook->setAuthor($faker->name());
+            $ebook->setIsValidated(false);
+            $ebook->setSlug($this->slugifyService->generate($ebook->getTitle()));
+            $ebook->setExpertise($this->getReference('expertise_' . rand(0, 5)));
+            $ebook->setUser($this->getReference('user_' . rand(0, 4)));
             $manager->persist($ebook);
             $this->addReference('ebook_' . $i, $ebook);
         }
